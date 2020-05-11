c3f30f058197a280e99e2545545691701ba057c1
cplank <cplankshepard@gmail.com>
2020-05-10 21:24:11 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RomanToInt("III");
+        }
+
+
+        public static void RomanToInt(string s)
+        {
+            string[] sArray = s.Split("");
+            Console.WriteLine(sArray);
+
+        }
+    }
+}
+      
